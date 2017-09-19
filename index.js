d3b3bcecae0cc0e40098dbaec5cdcc97d63f1dc3
Cyrus Bowman <cyrusbow@gmail.com>
2017-09-19 15:27:01 -0400

    Bug fix.

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function privateIP() {
   return request.get('http://'+host+'/latest/meta-data/local-ipv4')
     .timeout(100)
     .then((ip) => {
-      if (typeof ip == 'string' && ip.length == 0) throw new Error('AWS returned invalid ip address.');
+      if (!ip || !ip.text || typeof ip.text != 'string' || ip.text.length == 0) throw new Error('AWS returned invalid ip address.');
       return ip;
     });
 }
